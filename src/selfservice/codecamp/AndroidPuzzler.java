d3b8bb2a2d3edb977ba7d9be11daaa8e153e3ff1
Joonas Muhonen <joonas.muhonen@elisa.fi>
2012-05-23 10:58:08 +0300

    Initial Android app.

---
+++ src/selfservice/codecamp/AndroidPuzzler.java
@@ -0,0 +1,13 @@
+package selfservice.codecamp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AndroidPuzzler extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
