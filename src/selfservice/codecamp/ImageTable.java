07a2cc70db3aae2ae970b5d817da195572fa8ac4
Tomi Takussaari <tomi.takussaari@elisa.fi>
2012-05-23 12:02:53 +0300

    added imagetable

--- src/selfservice/codecamp/ImageTable.java
+++ src/selfservice/codecamp/ImageTable.java
@@ -3,16 +3,18 @@ package selfservice.codecamp;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import android.graphics.Bitmap;
 public class ImageTable {
 
 	
-	private List<List<String>> tableContents = new ArrayList<List<String>>();
+	private List<List<Bitmap>> tableContents = new ArrayList<List<Bitmap>>();
 	
-	public void addRow(List<String> newRow) {
+	public void addRow(List<Bitmap> newRow) {
 		tableContents.add(newRow);
 	}
 	
-	public List<List<String>> getContents() {
+	public List<List<Bitmap>> getContents() {
 		return tableContents;
 	}
 	
