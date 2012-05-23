9042a761b1c6e04101a1e74edf5e7f718a0604f8
Joonas Muhonen <jtmuhone@gmail.com>
2012-05-23 13:28:53 +0300

    Added new rows.

--- src/selfservice/codecamp/AndroidPuzzler.java
+++ src/selfservice/codecamp/AndroidPuzzler.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.os.Bundle;
 import android.widget.HorizontalScrollView;
 import android.widget.TableLayout;
+import android.widget.TableRow;
 
 public class AndroidPuzzler extends Activity {
     /** Called when the activity is first created. */
@@ -14,12 +15,21 @@ public class AndroidPuzzler extends Activity {
 	@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        
+
+        TableLayout mainTable = new TableLayout(this);
+        TableRow refreshRow = new TableRow(this);
+        TableRow imagesRow = new TableRow(this);
+        TableRow submitRow = new TableRow(this);
+        mainTable.addView(refreshRow);
+        mainTable.addView(imagesRow);
+        mainTable.addView(submitRow);
+
         HorizontalScrollView scrollView = new HorizontalScrollView(this);
+        imagesRow.addView(scrollView);
         TableLayout table = new TableLayout(this);
         scrollView.addView(table);
         
-        setContentView(scrollView);
+        setContentView(mainTable);
         new PuzzleImageLoader(table,(Context)this).execute(IMAGE_GET_URL);
 
     }
